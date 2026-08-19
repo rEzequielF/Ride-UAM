10a95c1627c95c3ed76b925f995d9d84b15c5eb8
rEzequielF <refernandez@uamv.edu.ni>
2026-08-19 14:54:24 -0600

    useAuthStore actualizado
    otra actualizacion

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -5,7 +5,8 @@ interface UserProfile{
     id: string;
     fullName: string;
     email: string;
-    status: 'unveified' | 'pending' | 'verified' | 'suspended';
+    accountStatus: 'active' | 'suspended';
+    driverStatus: 'none' | 'pending' | 'verified' | 'suspended';
 }
 
 interface AuthState{
@@ -14,8 +15,7 @@ interface AuthState{
     profile: UserProfile | null;
     isAuthenticated: boolean;
     isLoading: boolean;
-    setSession: (session: Session | null) => void;
-    setUser: (user: User | null) => void;
+    setAuthState: (session: Session | null, user: User | null) => void;
     setProfile: (profile: UserProfile | null) => void;
     setIsLoading: (isLoading: boolean) => void;
     signOut: () => void;
@@ -26,10 +26,18 @@ export const useAuthStore = create<AuthState>((set) => ({
     user: null,
     profile: null,
     isAuthenticated: false,
-    isLoading: true, // Verifica la sesion almacenada
-    setSession: (session) => set({ session, isAuthenticated: !!session }),
-    setUser: (user) => set({ user }),
+    isLoading: true,
+    setAuthState: (session, user) => set({
+        sesssion,
+        user,
+        isAuthenticated: !!session && !!user
+    }),
     setProfile: (profile) => set({ profile }),
     setIsLoading: (isLoading) => set({ isLoading }),
-    signOut: () => set({ session: null, user: null, profile: null, isAuthenticated: false }),
+    signOut: () => set({
+        session: null,
+        user: null,
+        profile: null,
+        isAuthenticated: false
+    }),
 }));
