02100873480eda625b19cee2801112cac4851162
rEzequielF <refernandez@uamv.edu.ni>
2026-08-19 14:15:24 -0600

    useAuthStore actualizado
    Actualizo useAuthStore.ts para conectaralo con authService, useAuth y supabase

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,22 +1,35 @@
 import { create } from 'zustand';
+import { Session, User } from '@supabase/supabase.js'
 
 interface UserProfile{
-    cif: string;
+    id: string;
     fullName: string;
     email: string;
-    status: 'No Verificado' | 'Pendiente' |  'Verificado' | 'Suspendido';
+    status: 'unveified' | 'pending' | 'verified' | 'suspended';
 }
 
 interface AuthState{
+    session: Session | null;
+    user: User | null;
     profile: UserProfile | null;
     isAuthenticated: boolean;
+    isLoading: boolean;
+    setSession: (session: Session | null) => void;
+    setUser: (user: User | null) => void;
     setProfile: (profile: UserProfile | null) => void;
-    logout: () => void;
+    setIsLoading: (isLoading: boolean) => void;
+    signOut: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
+    session: null,
+    user: null,
     profile: null,
     isAuthenticated: false,
-    setProfile: (profile) => set({ profile, isAuthenticated: !!profile }),
-    logout: () => set({ profile: null, isAuthenticated: false }),
+    isLoading: true, // Verifica la sesion almacenada
+    setSession: (session) => set({ session, isAuthenticated: !!session }),
+    setUser: (user) => set({ user }),
+    setProfile: (profile) => set({ profile }),
+    setIsLoading: (isLoading) => set({ isLoading }),
+    signOut: () => set({ session: null, user: null, profile: null, isAuthenticated: false }),
 }));
