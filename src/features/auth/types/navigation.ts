b25d3a5ac92607570bf7f828ea0b2fa7d2fa6957
rEzequielF <refernandez@uamv.edu.ni>
2026-08-23 17:21:38 -0600

    creacion de navigation.ts
    vamo avanzando loco si se puede

---
+++ src/features/auth/types/navigation.ts
@@ -0,0 +1,16 @@
+export type AuthStackParamList = {
+    Login: undefined;
+    Register: undefined;
+};
+
+export type MainTabParamList ={
+    Home: undefined;
+    Buscar: undefined;
+    MisRides: undefined;
+    Perfil: undefined;
+};
+
+export type RootStackParamList = {
+    Auth: undefined;
+    App: undefined;
+};
